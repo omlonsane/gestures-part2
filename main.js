596dbb96170a172fccc8e4dfbb30646d366d440b
omlonsane <90338824+omlonsane@users.noreply.github.com>
2022-03-31 21:13:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+prediction="";
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+
+});
+
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot()
+    {
+Webcam.snap(function(data_uri)
+{
+    document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
+});
+
+
+    }
+    console.log('ml5 version:',ml5.version);
+
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/f5ACVQMOz/model.json',modelloaded);
+function modelloaded(){
+    console.log("modelloaded");
+
+}
+
+function speak()
+{
+    var synth=window.speechSynthesis;
+    speak_data="The gesture is"+prediction;
+    var utterthis=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterthis);
+    
+}
+function check()
+{
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotresult);
+
+}
+function gotresult(error,results)
+{
+    if(error)
+    {
+        console.log(error);l
+    }
+        else{
+            console.log(results);
+            document.getElementById("result_gesture_name").innerHTML=results[0].label;
+            prediction=results[0].label;
+            speak();
+
+            if(results[0].label=="amazing")
+            {
+                document.getElementById("update_emoji").innerHTML="&#128076;";
+
+            }
+            if(results[0].label=="victory")
+            {
+                document.getElementById("update_emoji").innerHTML="&#9996;";
+
+            }
+            if(results[0].label=="best")
+            {
+                document.getElementById("update_emoji").innerHTML="&#128077;";
+
+            }
+        }
+   
+}
